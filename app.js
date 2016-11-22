f933fb4893271df8e06d9fb0fdcfefa789c3bd68
Joe Spedale <jspedale@gmail.com>
2016-11-22 18:43:33 -0500

    Minor refactoring and cleanup

--- app.js
+++ app.js
@@ -1,58 +1,52 @@
 $(document).ready(function () {
-  var lat;
-  var lon;
   if (navigator.geolocation) {
    navigator.geolocation.getCurrentPosition(function(position) {
-      $(".data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
+      $(".data").html("latitude: "+position.coords.latitude+"<br>longitude: "
+      +position.coords.longitude);
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
       getWeather(lat, lon);
-
     });
   };
 
   function getWeather(latitude, longitude) {
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=7ed6f77ca2de4ed775687d69ab17660e",
-    function(json) {
-    var location = json.name;
-    var condition = json.weather[0].description;
-    var tempK = json.main.temp;
-    var tempF = kelvinToFahrenheit(tempK);
-    var tempC = kelvinToCelsius(tempK);
-    console.log(location);
-    console.log(condition);
-    console.log(tempK);
-    console.log(tempF);
-    console.log(tempC);
-    tempColorChanger(tempF);
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+latitude
+    +"&lon="+longitude+"&appid=7ed6f77ca2de4ed775687d69ab17660e",
+      function(json) {
+      var location = json.name;
+      var condition = json.weather[0].description;
+      var tempK = json.main.temp;
+      var tempF = kelvinToFahrenheit(tempK);
+      var tempC = kelvinToCelsius(tempK);
+      tempColorChanger(tempF);
 
-    $('.location').html(location);
-    $('.condition').html(condition);
-    $('.celsius').html(tempC + "&deg; c");
-    $('.temp').html(tempF +"&deg; f");
-    $('.temp').click(function() {
-      $('.celsius').show();
-      $('.temp').hide();
-    });
-    $('.celsius').click(function() {
-      $('.temp').show();
-      $('.celsius').hide();
-    });
-  });
+      $('.location').html(location);
+      $('.condition').html(condition);
+      $('.celsius').html(tempC + "&deg; c");
+      $('.temp').html(tempF +"&deg; f");
 
+      // toggle for Celsius and Fahrenheit
+      $('.temp').click(function() {
+        $('.celsius').show();
+        $('.temp').hide();
+      });
+      $('.celsius').click(function() {
+        $('.temp').show();
+        $('.celsius').hide();
+      });
+    });
   }
 
 // converts Kelvin to Fahrenheit and rounds to the first decimal.
 function kelvinToFahrenheit(kelvin) {
-  var result = kelvin * (9/5) - 459.67;
-  return Math.round(result * 10) / 10;
+  return Math.round((kelvin * (9/5) - 459.67) * 10) / 10;
 }
 // converts Kelvin to Celsius and rounds to the first decimal.
 function kelvinToCelsius(kelvin) {
-  var result = kelvin - 273.15;
-  return Math.round(result * 10) / 10;
+  return Math.round((kelvin - 273.15) * 10) / 10;
 }
 
+// changes the color based on the temperature.
 function tempColorChanger(temp) {
   if (temp <= 32) {
     $('.bottomblock').css('color', '#325184');
