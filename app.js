528f5521350fe6977a588afecc48a68c8ef81d60
Joe Spedale <jspedale@gmail.com>
2016-11-22 15:45:44 -0500

    Added basic functionality and layout

--- app.js
+++ app.js
@@ -13,16 +13,43 @@ $(document).ready(function () {
 
   function getWeather(latitude, longitude) {
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=7ed6f77ca2de4ed775687d69ab17660e",
-        function(json) {
-        var name = json.name;
-        var condition = json.weather[0].description;
-        console.log(name);
-        console.log(condition);
-    $('.weather').on('click', function () {
-      console.log("hi");
-    });
-  });
-}
-  //getWeather(40, 30);
+    function(json) {
+    var location = json.name;
+    var condition = json.weather[0].description;
+    var tempK = json.main.temp;
+    var tempF = kelvinToFahrenheit(tempK);
+    var tempC = kelvinToCelsius(tempK);
+    console.log(location);
+    console.log(condition);
+    console.log(tempK);
+    console.log(tempF);
+    console.log(tempC);
 
-  });
+    $('.location').html(location);
+    $('.condition').html(condition);
+    $('.temp').html(tempF +"&deg;");
+    // $('.temp c').html(tempC + "&deg;");
+    // $('.temp').toggleClass('c');
+    $('p').toggle(function() {
+      $('.temp').html(tempC + "&deg;");
+      $('p').html('to C');
+    }, function() {
+      $('p').html('to F');
+      $('.temp').html(tempF + "&deg;");
+    });
+
+    });
+  }
+
+// converts Kelvin to Fahrenheit and rounds to the first decimal.
+function kelvinToFahrenheit(kelvin) {
+  var result = kelvin * (9/5) - 459.67;
+  return Math.round(result * 10) / 10;
+}
+// converts Kelvin to Celsius and rounds to the first decimal.
+function kelvinToCelsius(kelvin) {
+  var result = kelvin - 273.15;
+  return Math.round(result * 10) / 10;
+}
+
+});
